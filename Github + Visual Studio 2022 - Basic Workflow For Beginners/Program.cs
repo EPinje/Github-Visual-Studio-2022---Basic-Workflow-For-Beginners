2e1cfc69dcd26754a1f6811eca2871db82153c8d
Jonas Christian Larsen <jonaschristianlarsen@gmail.com>
2026-05-22 21:00:36 +0200

    Add project files.

---
+++ Github + Visual Studio 2022 - Basic Workflow For Beginners/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github___Visual_Studio_2022___Basic_Workflow_For_Beginners
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub!");
+        }
+    }
+}
