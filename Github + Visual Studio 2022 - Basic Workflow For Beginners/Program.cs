936c5aa07b8df0f8daa74196a582ab765506be49
Jonas Christian Larsen <jonaschristianlarsen@gmail.com>
2026-05-23 10:39:20 +0200

    Initial commit

--- Github + Visual Studio 2022 - Basic Workflow For Beginners/Program.cs
+++ Github + Visual Studio 2022 - Basic Workflow For Beginners/Program.cs
@@ -10,20 +10,11 @@ namespace Github___Visual_Studio_2022___Basic_Workflow_For_Beginners
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub!");
-            Console.WriteLine("Trying a commit from VS");
-            Console.WriteLine("Commit from GitHub");
-            Console.WriteLine("Hello GitHub!");
-            Console.WriteLine("Trying a commit from VS");
-            Console.WriteLine("Commit from GitHub"); Console.WriteLine("Hello GitHub!");
-            Console.WriteLine("Trying a commit from VS");
-            Console.WriteLine("Commit from GitHub"); Console.WriteLine("Hello GitHub!");
-            Console.WriteLine("Trying a commit from VS");
-            Console.WriteLine("Commit from GitHub"); Console.WriteLine("Hello GitHub!");
-            Console.WriteLine("Trying a commit from VS");
-            Console.WriteLine("Commit from GitHub"); Console.WriteLine("Hello GitHub!");
-            Console.WriteLine("Trying a commit from VS");
-            Console.WriteLine("Commit from GitHub");
+            //branch test
+
+            //branch test 2
+
+            //branch test 3
         }
     }
 }
